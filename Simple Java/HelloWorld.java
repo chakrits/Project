25a994606af3593cbc8098a90771e2203d9f70d7
Chakrit <37141743+chakrits@users.noreply.github.com>
2020-08-28 21:09:51 +0700

    Add files via upload

---
+++ Simple Java/HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import javax.swing.JOptionPane;
+public class HelloWorld
+{
+    
+    public static void main(String[] args) {
+        //JOptionPane.showMessageDialog(null,"Welcome");
+        System.out.println("Hello");
+    }
+}
