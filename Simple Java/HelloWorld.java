bbecbbdbc5b771f5accac8d29c6e312cca70257f
Chakrit <chakrit.salaeman@gmail.com>
2020-08-28 21:32:39 +0700

    update new code
    update

--- Simple Java/HelloWorld.java
+++ Simple Java/HelloWorld.java
@@ -6,11 +6,18 @@
  * @version (a version number or a date)
  */
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 public class HelloWorld
 {
     
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,"Welcome");
+        String name ;
+        Scanner sn = new Scanner(System.in);
+        System.out.print("Please put your name: ");
+        name = sn.nextLine();
+        //JOptionPane.showMessageDialog(null,name);
+
+        //JOptionPane.showMessageDialog(null,"Welcome");
         //System.out.println("Hello");
     }
 }
