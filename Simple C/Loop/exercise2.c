82276ff8c97e9556634851625bdacc90e25ad6e7
Chakrit <chakrit.salaeman@gmail.com>
2021-04-11 21:00:46 +0700

    add exercise2

---
+++ Simple C/Loop/exercise2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (void){
+    //Exercise for coding For-loop technique
+
+    int i, j , row;
+
+    puts("This code for practicing");
+
+
+    printf("\nPlease put the number of row : ");
+    scanf("%d",&row);
+
+    //
+    printf("Row is = %d \n",row);
+
+    //while-loop example 
+    //Control in vertical
+    i=0; 
+    while(i<=row)
+    {
+        j=0;
+        //Control in horizontal
+        while(j<=row)
+        {
+            //formula for print * in any shape
+            if(jj+i == row+1 || j==i)
+                printf("*");
+            else
+                printf(" ");
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    
+    return 0;
+    
+}
